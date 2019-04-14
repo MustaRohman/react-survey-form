ce3e42912e8bca65b08fa3e3e324129c592268a6
Musta Rohman <mustarohman@gmail.com>
2019-04-14 19:02:49 +0100

    Created Form feature component folder, with it's own js file and scss file

---
+++ src/form/Form.jsx
@@ -0,0 +1,14 @@
+import React, { Component} from 'react';
+import './Form.scss';
+
+class Form extends Component {
+    render() {
+        return (
+            <div className="Form">
+                FORM GOES HERE
+            </div>
+        )
+    }
+}
+
+export default Form;
