f3ed66323923822d573b94194effe62b284a4d03
Musta Rohman <mustarohman@gmail.com>
2019-04-14 20:25:54 +0100

    changed title of App.js and added instructions header

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ class App extends Component {
     render() {
         return (
             <div className="App">
-                <h1>Hello World!</h1>
+                <h1>Survey Form</h1>
+                <h2>Please enter your details in the form below</h2>
                 <Form></Form>
             </div>
         );
