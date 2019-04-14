40621969d99b9310d6ff99c76aa4df785d34a4ac
Musta Rohman <mustarohman@gmail.com>
2019-04-14 21:09:28 +0100

    added form element with example input element, along with onChange and form's onSubmit handle code

--- src/form/Form.jsx
+++ src/form/Form.jsx
@@ -2,11 +2,37 @@ import React, { Component} from 'react';
 import './Form.scss';
 
 class Form extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            title: '',
+            name: '',
+            dob: '',
+            location: '',
+            dateTime: '',
+            userFeedback: '',
+        }
+    }
+
+    handleSubmit(event) {
+        console.log('submit', event);
+        event.preventDefault();
+    }
+
+    handleChange(event) {
+        console.log(event.target.value);
+        this.setState({title: event.target.value})
+    }
+
     render() {
         return (
-            <div className="Form">
-                FORM GOES HERE
-            </div>
+            <form className="Form" onSubmit={(e) => this.handleSubmit(e)}>
+                <label>
+                    Title:
+                    <input type="text" value={this.state.title} onChange={(e) => this.handleChange(e)}></input>
+                </label>             
+                <input type="submit" value="Submit"></input>
+            </form>
         )
     }
 }
